f2fea792c752be11556ea316d746469345690359
abbiebarnhardt19 <abbiebarnhardt@gmail.com>
2025-08-21 12:12:08 -0500

    Initial Commit

---
+++ Trendy Headaches/Trendy_HeadachesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trendy_HeadachesApp.swift
+//  Trendy Headaches
+//
+//  Created by Abigail Barnhardt on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Trendy_HeadachesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
