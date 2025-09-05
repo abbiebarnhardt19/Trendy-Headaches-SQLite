7df66e71fbead6db970ec9ba9e4c4bd4ed3beba8
abbiebarnhardt19 <abbiebarnhardt@gmail.com>
2025-09-05 13:44:10 -0500

    Fixed blob jumps

--- Trendy Headaches/Login/ForgotPasswordView3.swift
+++ Trendy Headaches/Login/ForgotPasswordView3.swift
@@ -30,13 +30,11 @@ struct ForgotPasswordView3: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                ZStack{
-                    
-                    // Background blobs (fixed)
+                VStack{
                     ParametricBlob(points:25, amplitude: 0.15)
                         .fill(Color(hex: accent))
                         .frame(width: 400, height: 300)
-                        .offset(x: -0, y: 430)   // use offset instead of position
+                        .offset(x: -0, y: 550)
                         .rotationEffect(.degrees(335))
                         .ignoresSafeArea()
                         .allowsHitTesting(false)
@@ -44,13 +42,13 @@ struct ForgotPasswordView3: View {
                     ParametricBlob(points: 25, amplitude: 0.15)
                         .fill(Color(hex: accent))
                         .frame(width: 500, height: 300)
-                        .offset(x: 30, y: 420)   // use offset instead of position
+                        .offset(x: 30, y: 550)
                         .rotationEffect(.degrees(160))
                         .ignoresSafeArea()
                         .allowsHitTesting(false)
-                    
                 }
-                .ignoresSafeArea(.keyboard)
+                
+                
                 VStack(alignment: .trailing, spacing: 30) {
                     // Title pinned top-left
                     Text("Last Step")
@@ -58,65 +56,66 @@ struct ForgotPasswordView3: View {
                         .foregroundColor(Color(hex: accent))
                         .frame(width: 100, alignment: .trailing)
                         .padding(.trailing, 80)
-                        .padding(.top, 10)
+                        .padding(.top, 70)
                     
                     
                     // Question + input + button
-                    VStack() {
-                        CustomText(text: "New Password", color: accent)
-                            .padding(.leading, 60)
-                            .padding(.top, 40)
-                        
-                        SecureField("", text: $password_one)
-                            .textFieldStyle(CustomTextField(background: background, accent: accent, height: 60, width: 350))
-                        
-                        
-                        if !DatabaseManager.isPasswordValid(password_one) && !password_one.isEmpty {
-                            CustomWarningText(text: "Password must be at least 8 characters, contain uppercase, lowercase, number, and special character.")
-                                .padding(.leading, 20)
-                                .padding(.bottom, 5)
+                    GeometryReader { geo in
+                        VStack() {
+                            CustomText(text: "New Password", color: accent)
+                                .padding(.leading, 60)
+                                .padding(.top, 40)
+                            
+                            SecureField("", text: $password_one)
+                                .textFieldStyle(CustomTextField(background: background, accent: accent, height: 60, width: 350))
+                            
+                            
+                            if !DatabaseManager.isPasswordValid(password_one) && !password_one.isEmpty {
+                                CustomWarningText(text: "Password must be at least 8 characters, contain uppercase, lowercase, number, and special character.")
+                                    .padding(.leading, 20)
+                                    .padding(.bottom, 5)
+                            }
+                            
+                            else if !password_one.isEmpty && DatabaseManager.hashString(password_one) == currentPassword {
+                                CustomWarningText(text: "New password must be different from previous password.")
+                                    .padding(.leading, 20)
+                                    .padding(.bottom, 10)
+                            }
+                            else{
+                                CustomWarningText(text: "                                                                                    ")
+                                    .padding(.leading, 20)
+                                    .padding(.bottom, 20)
+                            }
+                            
+                            CustomText(text: "Confirm New Password", color: accent)
+                                .padding(.leading, 60)
+                            
+                            SecureField("", text: $password_two)
+                                .textFieldStyle(CustomTextField(background: background, accent: accent, height: 60, width: 350))
+                            
+                            if !password_two.isEmpty && password_two != password_one {
+                                CustomWarningText(text: "Passwords do not match.")
+                                    .padding(.leading, 20)
+                            }
+                            else{
+                                CustomWarningText(text: " ")
+                            }
+                            
+                            CustomButton(text: "Reset Password", background: background, accent: accent, height: 50, width: 200) {
+                                isPasswordUpdated = DatabaseManager.resetPassword(enteredEmail: enteredEmail, password_one: password_one)
+                            }
+                            .padding(.top, 15)
+                            .padding(.bottom, 150)
+                            .disabled(!passwordResetValid)
+                            .opacity(passwordResetValid ? 1.0 : 0.5)
+                            
+                            // Invisible NavigationLink that triggers when update is successful
+                            .navigationDestination(isPresented: $isPasswordUpdated) {
+                                LoginView()
+                            }
                         }
-                        
-                        else if !password_one.isEmpty && DatabaseManager.hashString(password_one) == currentPassword {
-                            CustomWarningText(text: "New password must be different from previous password.")
-                                .padding(.leading, 20)
-                                .padding(.bottom, 10)
-                        }
-                        else{
-                            CustomWarningText(text: "                                                                                    ")
-                                .padding(.leading, 20)
-                                .padding(.bottom, 20)
-                        }
-                        
-                        CustomText(text: "Confirm New Password", color: accent)
-                            .padding(.leading, 60)
-                        
-                        SecureField("", text: $password_two)
-                            .textFieldStyle(CustomTextField(background: background, accent: accent, height: 60, width: 350))
-                        
-                        if !password_two.isEmpty && password_two != password_one {
-                            CustomWarningText(text: "Passwords do not match.")
-                                .padding(.leading, 20)
-                        }
-                        else{
-                            CustomWarningText(text: " ")
-                        }
-                        
-                        CustomButton(text: "Reset Password", background: background, accent: accent, height: 50, width: 200) {
-                            isPasswordUpdated = DatabaseManager.resetPassword(enteredEmail: enteredEmail, password_one: password_one)
-                        }
-                        .padding(.top, 15)
-                        .padding(.bottom, 150)
-                        .disabled(!passwordResetValid)
-                        .opacity(passwordResetValid ? 1.0 : 0.5)
-                        
-                        // Invisible NavigationLink that triggers when update is successful
-                        .navigationDestination(isPresented: $isPasswordUpdated) {
-                            LoginView()
-                        }
-                        
+                        .padding(.top, geo.safeAreaInsets.top - 44) // ✅ now valid
                     }
-                    
                 }
                 
             }
